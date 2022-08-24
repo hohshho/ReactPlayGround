d437007e7d08c964cdcb15cd9f4492067681bd85
hohshho <tkdgur8377@gmail.com>
2022-08-25 00:49:03 +0900

    forward ref

--- udemyReactStudyStep5/src/components/Login/Login.js
+++ udemyReactStudyStep5/src/components/Login/Login.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import AuthContext from '../../store/auth-context';
+import Input from '../UI/Input/Input';
 
 // 매개 변수 1. 최신 state, dispatch된 action
 const emailReducer = (state, action) => {
@@ -35,6 +36,9 @@ const Login = (props) => {
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
 
     const authCtx = useContext(AuthContext);
+
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
 
     // State 안에 원하는 값을 가져와 변수로 만듬
     const { isValid: emailIsValid } = emailState;
@@ -74,34 +78,41 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        authCtx.onLogin(emailState.value, passwordState.value);
+
+        if (formIsValid) {
+            authCtx.onLogin(emailState.value, passwordState.value);
+        } else if (!emailIsValid) {
+            emailInputRef.current.focus();
+        } else {
+            passwordInputRef.current.focus();
+        }
     };
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                <div className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''}`}>
-                    <label htmlFor="email">E-Mail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={emailState.value}
-                        onChange={emailChangeHandler}
-                        onBlur={validateEmailHandler}
-                    />
-                </div>
-                <div className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''}`}>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={passwordState.value}
-                        onChange={passwordChangeHandler}
-                        onBlur={validatePasswordHandler}
-                    />
-                </div>
+                <Input
+                    ref={emailInputRef}
+                    id="email"
+                    label="E-Mail"
+                    type="email"
+                    isValid={emailIsValid}
+                    value={emailState.value}
+                    onChange={emailChangeHandler}
+                    onBlur={validateEmailHandler}
+                />
+                <Input
+                    ref={passwordInputRef}
+                    id="password"
+                    label="Password"
+                    type="password"
+                    isValid={passwordIsValid}
+                    value={passwordState.value}
+                    onChange={passwordChangeHandler}
+                    onBlur={validatePasswordHandler}
+                />
                 <div className={classes.actions}>
-                    <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+                    <Button type="submit" className={classes.btn}>
                         Login
                     </Button>
                 </div>
