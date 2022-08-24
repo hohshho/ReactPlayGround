c1dbf5adfba6ee4ca6378c9be1ad4a60a7ee5528
hohshho <tkdgur8377@gmail.com>
2022-08-24 22:43:46 +0900

    useReducer, Context API provider, consumer

--- udemyReactStudyStep5/src/components/Login/Login.js
+++ udemyReactStudyStep5/src/components/Login/Login.js
@@ -1,72 +1,98 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+// 매개 변수 1. 최신 state, dispatch된 action
+const emailReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.includes('@') };
+    } else if (action.type === 'INPUT_BLUR') {
+        return { value: state.value, isValid: state.value.includes('@') };
+    }
+    return { value: '', isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.trim().length > 6 };
+    } else if (action.type === 'INPUT_BLUR') {
+        return { value: state.value, isValid: state.value.trim().length > 6 };
+    }
+    return { value: '', isValid: false };
+};
+
 const Login = (props) => {
-    const [enteredEmail, setEnteredEmail] = useState('');
-    const [emailIsValid, setEmailIsValid] = useState();
-    const [enteredPassword, setEnteredPassword] = useState('');
-    const [passwordIsValid, setPasswordIsValid] = useState();
+    // const [enteredEmail, setEnteredEmail] = useState('');
+    // const [emailIsValid, setEmailIsValid] = useState();
+    // const [enteredPassword, setEnteredPassword] = useState('');
+    // const [passwordIsValid, setPasswordIsValid] = useState();
     const [formIsValid, setFormIsValid] = useState(false);
 
-    useEffect(() => {
-        console.log('effect');
-    }, []);
+    const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
+    const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
 
+    // State 안에 원하는 값을 가져와 변수로 만듬
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
+
+    // {}IsValid 값의 변화로 트래킹 가능
     useEffect(() => {
         const identifier = setTimeout(() => {
-            console.log('check validity!');
-            setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+            setFormIsValid(emailIsValid && passwordIsValid);
         }, 500);
 
         return () => {
             console.log('clean Up');
             clearTimeout(identifier);
         };
-    }, [enteredEmail, enteredPassword]);
+    }, [emailIsValid, passwordIsValid]);
 
     const emailChangeHandler = (event) => {
-        setEnteredEmail(event.target.value);
+        dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
+        // setFormIsValid(emailState.isValid && passwordState.isValid);
     };
 
     const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
+        dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
+
+        // setFormIsValid(emailState.isValid && passwordState.isValid);
     };
 
     const validateEmailHandler = () => {
-        setEmailIsValid(enteredEmail.includes('@'));
+        dispatchEmail({ type: 'INPUT_BLUR' });
     };
 
     const validatePasswordHandler = () => {
-        setPasswordIsValid(enteredPassword.trim().length > 6);
+        dispatchPassword({ type: 'INPUT_BLUR' });
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(enteredEmail, enteredPassword);
+        props.onLogin(emailState.value, passwordState.value);
     };
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                <div className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''}`}>
+                <div className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''}`}>
                     <label htmlFor="email">E-Mail</label>
                     <input
                         type="email"
                         id="email"
-                        value={enteredEmail}
+                        value={emailState.value}
                         onChange={emailChangeHandler}
                         onBlur={validateEmailHandler}
                     />
                 </div>
-                <div className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''}`}>
+                <div className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''}`}>
                     <label htmlFor="password">Password</label>
                     <input
                         type="password"
                         id="password"
-                        value={enteredPassword}
+                        value={passwordState.value}
                         onChange={passwordChangeHandler}
                         onBlur={validatePasswordHandler}
                     />
---
+++ udemyReactStudyStep5/src/store/auth-context.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthContext = React.createContext({
+    iosLoggedIn: false,
+});
+
+export default AuthContext;
