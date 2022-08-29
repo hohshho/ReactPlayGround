6588b886a3197eef974ada9f79b9e09917edef5c
hohshho <tkdgur8377@gmail.com>
2022-08-29 17:14:15 +0900

    add cart action, header button event

--- udemyReactStudyStep6/src/Components/store/CartProvider.js
+++ udemyReactStudyStep6/src/Components/store/CartProvider.js
@@ -10,8 +10,44 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
         // concat으로 기존 배열에 값을 추가하면 새로운 객체로 값을 생성해서 이전 상태를 바꾸지 않아도 된다.
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
+    }
+
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
 
         return {
             items: updatedItems,
