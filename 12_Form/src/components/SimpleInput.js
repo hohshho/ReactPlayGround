8fdc48d8b4f465da11084f7519bf23ff80036b27
hohshho <tkdgur8377@gmail.com>
2022-09-02 23:59:28 +0900

    input태그 state, valid, handler추가

--- 12_Form/src/components/SimpleInput.js
+++ 12_Form/src/components/SimpleInput.js
@@ -1,15 +1,90 @@
+import { useEffect, useState } from 'react';
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+    const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
+    const enteredNameIsValid = enteredName.trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    const enteredEmailIsValid = enteredEmail.includes('@');
+    const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
+    let formIsValid = false;
+
+    if (enteredNameIsValid && enteredEmailIsValid) {
+        formIsValid = true;
+    }
+
+    const nameInputChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    };
+
+    const emailInputChangeHandler = (event) => {
+        setEnteredEmail(event.target.value);
+    };
+
+    const nameInputBlurHandler = (event) => {
+        setEnteredNameTouched(true);
+    };
+
+    const emailInputBlurHandler = (event) => {
+        setEnteredEmailTouched(true);
+    };
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault();
+
+        setEnteredNameTouched(true);
+
+        if (!enteredNameIsValid) {
+            return;
+        }
+
+        // Ref로 직접 DOM을 조작하는 방법은 좋은 방법이 아님 -> 직접 상태 변경하는 방법 추천
+        // nameInputRef.current.value = '';
+        setEnteredName('');
+        setEnteredNameTouched(false);
+
+        setEnteredEmail('');
+        setEnteredEmailTouched(false);
+    };
+
+    const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+    const emailInputClasses = emailInputIsInvalid ? 'form-control invalid' : 'form-control';
+
+    return (
+        <form onSubmit={formSubmissionHandler}>
+            {/* 문제 1. 이메일 입력 폼 만들어서 두개 같이 검증하게*/}
+            <div className={emailInputClasses}>
+                <label htmlFor="email">Your Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={enteredEmail}
+                    onChange={emailInputChangeHandler}
+                    onBlur={emailInputBlurHandler}
+                />
+                {emailInputIsInvalid && <p className="error-text">Name must not be empty</p>}
+            </div>
+            <div className={nameInputClasses}>
+                <label htmlFor="name">Your Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={enteredName}
+                    onChange={nameInputChangeHandler}
+                    onBlur={nameInputBlurHandler}
+                />
+                {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
+            </div>
+            <div className="form-actions">
+                <button disabled={!formIsValid}>Submit</button>
+            </div>
+        </form>
+    );
 };
 
 export default SimpleInput;
