c381876d939367386c11ad7b1c18be9146db0196
hohshho <tkdgur8377@gmail.com>
2022-09-02 13:25:42 +0900

    hooks 내부에서 사용하는 함수를 useCallback사용해 의존성 유지

---
+++ 11_CustomHooks2/src/hooks/use-https.js
@@ -0,0 +1,39 @@
+import { useCallback, useState } from 'react';
+
+const useHttp = (requestConfig, applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    // 1) 어떤 종류의 요청이든 가능하게
+    // 2) 어떤 데이터 변환도 가능하게
+    // 3) 로딩과 에러 상태 관리
+    const sendRequest = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(requestConfig.url, {
+                method: requestConfig.method ? requestConfig.method : 'GET',
+                headers: requestConfig.headers ? requestConfig.headers : {},
+                body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+            });
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+            applyData(data);
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
+        }
+        setIsLoading(false);
+    }, [requestConfig, applyData]);
+
+    return {
+        isLoading,
+        error,
+        sendRequest,
+    };
+};
+
+export default useHttp;
