7ac1e559fd3eba12ac803e56944cdb66ca6f2e2b
hohshho <tkdgur8377@gmail.com>
2022-12-28 16:21:10 +0900

    동기 작업 예제 추가

---
+++ 14_async/src/store/cart-slice.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartState = {
+    items: [],
+    totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: cartState,
+    reducers: {
+        addItemToCart(state, action) {
+            const newItem = action.payload;
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+            state.totalQuantity++;
+
+            if (!existingItem) {
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title,
+                });
+                return;
+            }
+
+            existingItem.quantity = existingItem.quantity + 1;
+            existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            state.totalQuantity--;
+
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter((item) => item.id !== id);
+                return;
+            }
+            existingItem.quantity--;
+            // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+        },
+    },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
