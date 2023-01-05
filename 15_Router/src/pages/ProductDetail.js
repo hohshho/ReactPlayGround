78973cc69a1ff9bcaa7dcae528916771eaee2e03
hohshho <tkdgur8377@gmail.com>
2023-01-05 16:52:23 +0900

    router 마무리

--- 15_Router/src/pages/ProductDetail.js
+++ 15_Router/src/pages/ProductDetail.js
@@ -1,9 +1,14 @@
-import { useParams, useLocation, useSearchParams } from 'react-router-dom';
+import { useParams, useLocation, useSearchParams, useNavigate } from 'react-router-dom';
 
 const ProductDetail = () => {
     const params = useParams();
     const location = useLocation();
     const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const clickHandler = () => {
+        navigate('/', { replace: true });
+    };
 
     console.log(params);
     console.log(location);
@@ -14,6 +19,7 @@ const ProductDetail = () => {
         <section>
             <h1>Product Detail</h1>
             <p>{params.productId}</p>
+            <button onClick={clickHandler}>click</button>
         </section>
     );
 };
